9fecf022d0844faf73248ae79d9b753ba00e85b9
type-donghyun <118829460+type-donghyun@users.noreply.github.com>
2023-03-09 17:36:03 +0900

    Add files via upload

--- Windows_Lisence_Key.bat
+++ Windows_Lisence_Key.bat
@@ -1,25 +1,30 @@
 @ECHO OFF
-::================================================================================관리자 권한 요청
+::_____________________________________________________________________________________________________________________________________________________________
+:: 관리자 권한 요청
+
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-IF %errorlevel% NEQ 0 (
+
+IF %errorlevel% neq 0 (
 	GOTO UACPrompt
 ) ELSE (
 	GOTO gotAdmin
 )
 
 :UACPrompt
-ECHO Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-ECHO UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
-"%temp%\getadmin.vbs"
-EXIT /B
+	ECHO SET UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+	ECHO UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+	"%temp%\getadmin.vbs"
+	EXIT /B
 
 :gotAdmin
-IF EXIST "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
-PUSHD "%CD%"
-CD /D "%~dp0"
-::====================================================================================================
+	IF EXIST "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+	PUSHD "%CD%"
+	CD /D "%~dp0"
 
-::================================================================================ECHO 색상 설정
+::_____________________________________________________________________________________________________________________________________________________________
+:: ECHO 색상 설정
+
 SET _elev=
 IF /i "%~1"=="-el" SET _elev=1
 FOR /f "tokens=6 delims=[]. " %%G in ('ver') do set winbuild=%%G
@@ -42,7 +47,8 @@ SET "EchoLightPurple=%_psc% write-host -back Black -fore Magenta"
 SET "EchoLightYellow=%_psc% write-host -back Black -fore Yellow"
 SET "EchoBrightWhite=%_psc% write-host -back Black -fore White"
 SET "ErrLine=echo: & %EchoRed% ==== ERROR ==== &echo:"
-::====================================================================================================
+
+::_____________________________________________________________________________________________________________________________________________________________
 
 FOR /f "tokens=4-6" %%a in ('systeminfo') do (
 	IF "%%a" equ "Windows" (
