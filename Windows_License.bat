f1db1949d94d65fffdea88e93e810312d3bf889e
type-donghyun <118829460+type-donghyun@users.noreply.github.com>
2024-03-26 12:44:57 +0900

    Update Windows_License.bat

--- Windows_License.bat
+++ Windows_License.bat
@@ -58,21 +58,18 @@ IF %version% equ 10 (
 )
 
 :dowork
-IF %edition% equ Pro (
+IF /I %edition% equ Pro (
     ECHO Windows %version% %edition%가 감지되었습니다.
     ECHO.
-    ECHO [1] 조직의 정품 인증 서비스를 사용하여 Windows 정품 인증(180일)
-    ECHO [2] 디지털 라이선스를 사용하여 정품 인증(무제한)
-    CHOICE /c 12
-    IF %errorlevel% equ 1 (
-        SLMGR /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
-    ) ELSE IF %errorlevel% equ 2 (
-        cscript //nologo %windir%\system32\slmgr.vbs /ato
-        cscript //nologo %windir%\system32\slmgr.vbs /ipk VK7JG-NPHTM-C97JM-9MPGT-3V66T
-    )
-) ELSE IF %edition% equ Home (
+    ECHO [1] 조직의 정품 인증 서비스를 사용하여 Windows 정품 인증
+    ECHO [2] 디지털 라이선스를 사용하여 정품 인증
+    GOTO :pro
+) ELSE IF /I %edition% equ Home (
     ECHO Windows %version% %edition%이 감지되었습니다.
-    SLMGR /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99
+    ECHO.
+    ECHO [1] 조직의 정품 인증 서비스를 사용하여 Windows 정품 인증
+    ECHO [2] 디지털 라이선스를 사용하여 정품 인증
+    GOTO :home
 ) ELSE (
     COLOR 04
     ECHO 활성화할 수 없는 Windows 버전입니다: %edition%
@@ -80,21 +77,57 @@ IF %edition% equ Pro (
     GOTO :workend
 )
 
-SLMGR /skms kms.digiboy.ir
-SLMGR /ato
-
-CHCP 65001 > nul
+:pro
+CHOICE /c 12
 CLS
-ECHO ▣ Windows 정품 활성화
+IF %errorlevel% equ 1 (
+    ECHO 조직의 정품 인증 서비스를 사용하여 Windows 정품 인증중
+    cscript //nologo %windir%\system32\slmgr.vbs /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+    GOTO :activate
+) ELSE IF %errorlevel% equ 2 (
+    ECHO 디지털 라이선스를 사용하여 정품 인증중
+    cscript //nologo %windir%\system32\slmgr.vbs /ipk NF6HC-QH89W-F8WYV-WWXV4-WFG6P
+    GOTO :activate
+) ELSE (
+    COLOR 04
+    ECHO 선택된 옵션이 잘못되었습니다.
+    TIMEOUT /t 2 > nul
+    GOTO :workend
+)
+
+:home
+CHOICE /c 12
+CLS
+IF %errorlevel% equ 1 (
+    cscript //nologo %windir%\system32\slmgr.vbs /ipk TX9XD-98N7V-6WMQ6-BX7FG-H8Q99
+    GOTO :activate
+) ELSE IF %errorlevel% equ 2 (
+    ECHO 아직 여기는 미구현 ㅎㅎ ㅈㅅ
+    GOTO :activate
+) ELSE (
+    COLOR 04
+    ECHO 선택된 옵션이 잘못되었습니다.
+    TIMEOUT /t 2 > nul
+    GOTO :workend
+)
+
+:activate
+
+cscript //nologo %windir%\system32\slmgr.vbs /skms kms.digiboy.ir
+cscript //nologo %windir%\system32\slmgr.vbs /ato
+cscript //nologo %windir%\system32\slmgr.vbs /xpr
 ECHO.
-CHOICE /c 12 /n /t 3 /d 2 /m "라이센스 정보와 만료 날짜를 확인하시겠습니까? [1] Yes [2] No"
+ECHO 라이선스 정보와 만료 날짜를 확인
+ECHO [1] Yes
+ECHO [2] No
+CHOICE /c 12 /n /t 3 /d 2 
 
 CLS
 IF %errorlevel% equ 1 (
-    SLMGR /xpr
     SLMGR /dlv
 )
 
 :workend
+CLS
 ECHO 작업을 종료합니다.
 TIMEOUT /t 3 > nul
